7aaf70d5ffd5f58b2c7b72f014b46be3bb521457
WriteEnableQue, LaunchEnableQue, OpenEpicentre, OpenAvrilCFSD,  OpenAvrilNNI. <65234262+OpenAvril@users.noreply.github.com>
2026-05-06 11:55:39 +1000

    Add files via upload
    Signed-off-by: WriteEnableQue, LaunchEnableQue, OpenEpicentre, OpenAvrilCFSD,  OpenAvrilNNI. <65234262+OpenAvril@users.noreply.github.com>

---
+++ CLIB_WriteEnableForThreadsAt_STACK/cmake-build-debug/CMakeFiles/CLIB_WriteEnableForThreadsAt_STACK.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CLIB_WriteEnableForThreadsAt_STACK.dir/src/CLIB_WriteEnableForThreadsAt_STACK.cpp.o"
+  "CMakeFiles/CLIB_WriteEnableForThreadsAt_STACK.dir/src/CLIB_WriteEnableForThreadsAt_STACK.cpp.o.d"
+  "CMakeFiles/CLIB_WriteEnableForThreadsAt_STACK.dir/src/WriteEnableForThreadsAt_STACK.cpp.o"
+  "CMakeFiles/CLIB_WriteEnableForThreadsAt_STACK.dir/src/WriteEnableForThreadsAt_STACK.cpp.o.d"
+  "CMakeFiles/CLIB_WriteEnableForThreadsAt_STACK.dir/src/WriteEnableForThreadsAt_STACK_Control.cpp.o"
+  "CMakeFiles/CLIB_WriteEnableForThreadsAt_STACK.dir/src/WriteEnableForThreadsAt_STACK_Control.cpp.o.d"
+  "CMakeFiles/CLIB_WriteEnableForThreadsAt_STACK.dir/src/WriteEnableForThreadsAt_STACK_Framework.cpp.o"
+  "CMakeFiles/CLIB_WriteEnableForThreadsAt_STACK.dir/src/WriteEnableForThreadsAt_STACK_Framework.cpp.o.d"
+  "CMakeFiles/CLIB_WriteEnableForThreadsAt_STACK.dir/src/WriteEnableForThreadsAt_STACK_Global.cpp.o"
+  "CMakeFiles/CLIB_WriteEnableForThreadsAt_STACK.dir/src/WriteEnableForThreadsAt_STACK_Global.cpp.o.d"
+  "libCLIB_WriteEnableForThreadsAt_STACK.a"
+  "libCLIB_WriteEnableForThreadsAt_STACK.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLIB_WriteEnableForThreadsAt_STACK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
