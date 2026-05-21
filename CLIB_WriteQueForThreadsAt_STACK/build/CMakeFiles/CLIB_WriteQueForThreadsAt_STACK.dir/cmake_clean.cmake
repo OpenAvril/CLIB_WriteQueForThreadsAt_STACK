5901c213cf464e99fbb72fd80d04d1209364ddf2
WriteEnableQue, LaunchEnableQue, OpenEpicentre, OpenAvrilCFSD,  OpenAvrilNNI. <Jasper.Assembly@icloud.com>
2026-05-21 11:04:36 +1000

    Add files via upload
    DEVELOPMENT_2026_05_21_1102, <COMPILE_SUCCESS>.
    
    implemented:
    - CLion new C++ Library project.
    - fixed login in update que.
    - added global states of write control.
    
    Signed-off-by: WriteEnableQue, LaunchEnableQue, OpenEpicentre, OpenAvrilCFSD,  OpenAvrilNNI. <Jasper.Assembly@icloud.com>

---
+++ CLIB_WriteQueForThreadsAt_STACK/build/CMakeFiles/CLIB_WriteQueForThreadsAt_STACK.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CLIB_WriteQueForThreadsAt_STACK.dir/link.d"
+  "CMakeFiles/CLIB_WriteQueForThreadsAt_STACK.dir/src/CLIB_WriteEnableForThreadsAt_STACK.cpp.o"
+  "CMakeFiles/CLIB_WriteQueForThreadsAt_STACK.dir/src/CLIB_WriteEnableForThreadsAt_STACK.cpp.o.d"
+  "CMakeFiles/CLIB_WriteQueForThreadsAt_STACK.dir/src/WriteEnableForThreadsAt_STACK.cpp.o"
+  "CMakeFiles/CLIB_WriteQueForThreadsAt_STACK.dir/src/WriteEnableForThreadsAt_STACK.cpp.o.d"
+  "CMakeFiles/CLIB_WriteQueForThreadsAt_STACK.dir/src/WriteEnableForThreadsAt_STACK_Control.cpp.o"
+  "CMakeFiles/CLIB_WriteQueForThreadsAt_STACK.dir/src/WriteEnableForThreadsAt_STACK_Control.cpp.o.d"
+  "CMakeFiles/CLIB_WriteQueForThreadsAt_STACK.dir/src/WriteEnableForThreadsAt_STACK_Framework.cpp.o"
+  "CMakeFiles/CLIB_WriteQueForThreadsAt_STACK.dir/src/WriteEnableForThreadsAt_STACK_Framework.cpp.o.d"
+  "CMakeFiles/CLIB_WriteQueForThreadsAt_STACK.dir/src/WriteEnableForThreadsAt_STACK_Global.cpp.o"
+  "CMakeFiles/CLIB_WriteQueForThreadsAt_STACK.dir/src/WriteEnableForThreadsAt_STACK_Global.cpp.o.d"
+  "libCLIB_WriteQueForThreadsAt_STACK.pdb"
+  "libCLIB_WriteQueForThreadsAt_STACK.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLIB_WriteQueForThreadsAt_STACK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
