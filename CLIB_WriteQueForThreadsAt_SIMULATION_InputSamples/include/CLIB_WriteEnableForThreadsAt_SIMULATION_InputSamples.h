67c028d00c140260e13f0989daeaacd6d4eacb78
OpenAvril <Jasper.Assembly@icloud.com>
2026-07-15 12:09:09 +1000

    DEVELOPMENT_2026_07_15_1208, COMPILE_SUCCESS>.
    initial commit.

---
+++ CLIB_WriteQueForThreadsAt_SIMULATION_InputSamples/include/CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples.h
@@ -0,0 +1,29 @@
+#ifndef CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples_H
+#define CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples_H
+#include "../include/WriteEnableForThreadsAt_SIMULATION_InputSamples__Framework.h"
+extern "C"
+{
+    namespace CLIBWriteQueAtSIMULATION
+    {
+        class CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples
+        {
+        public:
+            static void* CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples__App_FUNCT_generate_Program();
+            static unsigned char* CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples__App_FUNCT_get_flag_isPGM_INSTANTIATED();
+            static void CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples__App_FUNCT_terminate_Program();
+            static void CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples__App_FUNCT_write_End(unsigned char* bytes);
+            static void CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples__App_FUNCT_write_Start(unsigned char* bytes);
+        private:
+		    static void CLIB_WriteEnableForThreadsAt_ServerOutputSend__dyn_APP_FUNCT_Calc_IsAllINSTANTIATED();
+            static void CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples__dyn_APP_FUNCT_Calc_IsAllINSTANTIATED();
+            static void CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples__stat_CLASS_boot1_DEFINE_Framework();
+            static void CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples__stat_CLASS_boot3_INITIALISE_Framework();
+            static class WriteEnableForThreadsAt_SIMULATION_InputSamples__Framework* CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples__stat_CLASS_get_ptr_Framework();
+            static void CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples__stat_REG_boot1_DEFINE_CLIBLaunchQueAtSIMULATION_isFlagINSTANTIATED();
+            static void CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples__stat_REG_boot2_SUBSTANTIATE_CLIBLaunchQueAtSIMULATION_isFlagINSTANTIATED();
+            static void CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples__stat_REG_boot3_INITIALISE_CLIBLaunchQueAtSIMULATION_isFlagINSTANTIATED();
+            static bool* CLIB_WriteEnableForThreadsAt_SIMULATION_InputSamples__stat_REG_get_CLIBLaunchQueAtSIMULATION_isFlagINSTANTIATED();
+        };
+    }
+}
+#endif
---
+++ CLIB_WriteQueForThreadsAt_SIMULATION_OutputSamples/src/WriteEnableForThreadsAt_SIMULATION_OutputSamples__App.cpp
@@ -0,0 +1,102 @@
+#include "../include/WriteEnableForThreadsAt_SIMULATION_OutputSamples__App.h"
+#include "../include/WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control.h"
+#include "../include/WriteEnableForThreadsAt_SIMULATION_OutputSamples__Framework.h"
+#include "../include/WriteEnableForThreadsAt_SIMULATION_OutputSamples__Global.h"
+#include <cstdint>
+#include <iostream>
+#include <ostream>
+    CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control* CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::_stat_CLASS_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control = NULL;
+    CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()
+    {
+        CLASS_boot0_DECLARE_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App();
+        CLASS_boot1_DEFINE_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App();
+        CLASS_boot3_INITIALISE_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App();
+    }
+    CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::~WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()
+    {
+        delete _stat_CLASS_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control;
+    }
+    void CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::CLASS_boot0_DECLARE_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()
+    {
+        std::cout << "entered CLASS_boot0_DECLARE_WriteEnableForThreadsAt_SIMULATION()" << std::endl;
+
+        std::cout << "exiting CLASS_boot0_DECLARE_WriteEnableForThreadsAt_SIMULATION()" << std::endl;
+    }
+    void CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::CLASS_boot1_DEFINE_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()
+    {
+        std::cout << "entered CLASS_boot1_DEFINE_WriteEnableForThreadsAt_SIMULATION" << std::endl;
+        stat_CLASS_boot1_DEFINE_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control();
+        std::cout << "exiting CLASS_boot1_DEFINE_WriteEnableForThreadsAt_SIMULATION" << std::endl;
+    }
+    void CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::CLASS_boot3_INITIALISE_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()
+    {
+        std::cout << "entered CLASS_boot3_INITIALISE_WriteEnableForThreadsAt_SIMULATION()" << std::endl;
+        stat_CLASS_boot3_INITIALISE_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control();
+        std::cout << "exiting CLASS_boot3_INITIALISE_WriteEnableForThreadsAt_SIMULATION()" << std::endl;
+    }
+    void CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::REG_boot0_DECLARE_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()
+    {
+        std::cout << "entered REG_boot0_DECLARE_WriteEnableForThreadsAt_SIMULATION()" << std::endl;
+
+        std::cout << "exiting REG_boot0_DECLARE_WriteEnableForThreadsAt_SIMULATION()" << std::endl;
+    }
+    void CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::REG_boot1_DEFINE_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App(WriteEnableForThreadsAt_SIMULATION_OutputSamples__Framework* obj)
+    {
+        std::cout << "entered REG_boot1_DEFINE_WriteEnableForThreadsAt_SIMULATION()" << std::endl;
+
+        std::cout << "exiting REG_boot1_DEFINE_WriteEnableForThreadsAt_SIMULATION()" << std::endl;
+    }
+    void CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::REG_boot2_SUBSTANTIATE_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App(WriteEnableForThreadsAt_SIMULATION_OutputSamples__Framework* obj)
+    {
+        std::cout << "entered REG_boot2_SUBSTANTIATE_WriteEnableForThreadsAt_SIMULATION()" << std::endl;
+
+        std::cout << "exiting REG_boot2_SUBSTANTIATE_WriteEnableForThreadsAt_SIMULATION()" << std::endl;
+    }
+    void CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::REG_boot3_INITIALISE_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App(WriteEnableForThreadsAt_SIMULATION_OutputSamples__Framework* obj)
+    {
+        std::cout << "entered REG_boot3_INITIALISE_WriteEnableForThreadsAt_SIMULATION()" << std::endl;
+
+        std::cout << "exiting REG_boot3_INITIALISE_WriteEnableForThreadsAt_SIMULATION()" << std::endl;
+    }
+    void CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::boot4_PGM_INSTANTIATION_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App(WriteEnableForThreadsAt_SIMULATION_OutputSamples__Framework* obj)
+    {
+        //if thread primed and system initialised, exit, else wait.
+    }
+    void CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::dyn_APP_FUNCT_write_End(WriteEnableForThreadsAt_SIMULATION_OutputSamples__Framework* obj, uint8_t coreId)
+    {
+        obj->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control()->dyn_REG_set_Item_On_list_Of_WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control__2ibt_flag_WriteState(coreId, obj->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__Global()->dyn_REG_get_ptr_CONST_WriteEnableForThreadsAt_SIMULATION_OutputSamples__2bitFLAG_IDLE());
+        obj->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control()->dyn_REG_set_WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control__new_writeCycle_Try_ThreadId_Index(obj->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control()->dyn_REG_get_Item_On_WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control__list_Of_WriteActive_Count_For_ThreadId(coreId) + 1);
+        if (obj->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control()->dyn_REG_get_Item_WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control__new_writeCycle_Try_ThreadId_Index() == 3)
+        {
+            obj->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control()->dyn_REG_set_WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control__new_writeCycle_Try_ThreadId_Index(0);
+        }
+        obj->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control()->dyn_APP_FUNCT_WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control__writeQue_Update(obj);
+        obj->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control()->dyn_APP_FUNCT_WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control__writeEnable_SortQue(obj);
+        obj->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control()->dyn_REG_set_flag_WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control__praisingWrite(false);
+    }
+    void CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::dyn_APP_FUNCT_write_Start(WriteEnableForThreadsAt_SIMULATION_OutputSamples__Framework* obj, uint8_t coreId)
+    {
+        obj->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control()->dyn_APP_FUNCT_WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control__writeEnable_Request(obj, coreId);
+        obj->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control()->dyn_APP_FUNCT_WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control__writeQue_Update(obj);
+        obj->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control()->dyn_APP_FUNCT_WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control__writeEnable_SortQue(obj);
+        obj->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App()->dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control()->dyn_APP_FUNCT_WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control__writeEnable_Activate(obj, coreId);
+    }
+    CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control* CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::dyn_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control()
+    {
+        return stat_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control();
+    }
+
+    void CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::stat_CLASS_boot1_DEFINE_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control()
+    {
+        _stat_CLASS_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control = NULL;
+    }
+
+    void CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::stat_CLASS_boot3_INITIALISE_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control()
+    {
+        _stat_CLASS_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control = new class CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control();
+        while (stat_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control() == NULL) {}
+    }
+    CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__Control* CLIBWriteQueAtSIMULATION::WriteEnableForThreadsAt_SIMULATION_OutputSamples__App::stat_CLASS_get_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control()
+    {
+        return _stat_CLASS_ptr_WriteEnableForThreadsAt_SIMULATION_OutputSamples__App__Control;
+    }
